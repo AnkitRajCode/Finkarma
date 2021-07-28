83ad9016811a108473f1a5e91081598f1a34f3c1
AnkitRajCode <rajankit432@gmail.com>
2021-07-29 02:08:59 +0530

    mobile view of header done

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import Finkarmadaily from '../components/Finkarmadaily';
+import '../css/Finkarmadaily.css';
+
 
 class Landing extends Component {
     render() {
